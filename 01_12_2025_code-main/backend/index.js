7a0a6570ed83e97951c5dba1046d671afb55fbcf
Namwhan111 <athitiya1467@gmail.com>
2026-03-13 20:37:02 +0700

    update project

--- 01_12_2025_code-main/backend/index.js
+++ 01_12_2025_code-main/backend/index.js
@@ -23,10 +23,10 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'attendance-system-phi-six.vercel.app',
+  origin: process.env.FRONTEND_URL || "https://attendance-system-phi-six.vercel.app",
   credentials: true
 }));
-attendance-system-phi-six.vercel.app
+
 // ===== API routes =====
 app.use(subjectRoute);
 app.use(stdRoute);
