3d8bb9ca0e43534f6f82fdba9524f520bbdbcec7
Namwhan111 <athitiya1467@gmail.com>
2026-03-13 15:15:16 +0700

    update project

--- 01_12_2025_code-main/backend/index.js
+++ 01_12_2025_code-main/backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://attendance-system-git-main-namwhan111s-projects.vercel.app/',
+  origin: process.env.FRONTEND_URL || 'https://attendance-system-git-main-namwhan111s-projects.vercel.app',
   credentials: true
 }));
 mini2-nine.vercel.app
